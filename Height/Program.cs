b29675303abe494030faba08bc37a60fc1f65988
Tyagi.Garvit <Tyagi.Garvit@INLPF2RX2LR>
2022-09-22 16:58:51 +0530

    Add project files.

---
+++ Height/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Height
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the height of the person (in centimetres):");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            if (height < 135.0)
+                Console.Write("The person is short \n");
+            else if (height >= 180.0)
+                Console.Write("The person is tall");
+            else
+                Console.Write("Average height.\n\n");
+        }
+    }
+}
